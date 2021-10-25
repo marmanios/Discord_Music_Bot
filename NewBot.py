01c050cf2d95ddd8ee96949850fa3043766c9402
Maged Armanios <67605395+marmanios@users.noreply.github.com>
2021-10-25 18:53:27 -0400

    Add files via upload

---
+++ NewBot.py
@@ -0,0 +1,131 @@
+import asyncio
+import discord
+from discord import channel
+from discord.ext import commands, tasks
+import random
+import youtube_dl
+import urllib.request
+import re
+
+def getUrl(url):
+    if "www.youtube.com/watch?" in url:
+        pass
+        print("pass")
+        return url
+    else:
+        video_ids = re.findall(r"watch\?v=(\S{11})", urllib.request.urlopen(("https://www.youtube.com/results?search_query="+url.replace(" ","+"))).read().decode())
+        return("https://www.youtube.com/watch?v=" + video_ids[0])
+    
+# Suppress noise about console usage from errors
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def join(self, ctx):
+        channel = ctx.author.voice.channel
+        await channel.connect()
+
+    @commands.command(aliases=["play"])
+
+    async def stream(self, ctx, *, url):
+        url = getUrl(url)
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+            ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+
+        await ctx.send(f'Now playing: {player.title}')
+
+
+    @commands.command(aliases=["leave"])
+    async def stop(self, ctx):
+        """Stops and disconnects the bot from voice"""
+
+        await ctx.voice_client.disconnect()
+
+    @commands.command()
+    async def pause(self, ctx):
+        """Stops and disconnects the bot from voice"""
+
+        await ctx.voice_client.pause()
+        #await ctx.send(f'Now pausing: {player.title}')
+
+    @commands.command()
+    async def resume(self, ctx):
+        """Stops and disconnects the bot from voice"""
+
+        await ctx.voice_client.resume()
+        
+    @stream.before_invoke
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("You are not connected to a voice channel.")
+                raise commands.CommandError("Author not connected to a voice channel.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
+                   description='Relatively simple music bot example')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+bot.add_cog(Music(bot))
+bot.run('OTAxMzQ4MTQ0OTU5MzUyODQy.YXOj3w.0TrYQq9rxL7GwHxQdNNxrLUN1pg')
+
+
+
+        
+
+
